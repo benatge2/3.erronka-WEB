27d73731550799af91be5fc58659d41aaeeddb15
Igor Viyuela <igorviyusan@gmail,com>
2024-03-18 12:37:27 +0100

    añadir public

--- src/views/supplier/carrusel-Premios.php
+++ src/views/supplier/carrusel-Premios.php
@@ -1,4 +1,4 @@
-<div class="mySlide">
+<div class="mySlide frame">
     <div class="priceImage">
         <img src="#" style="width:100%">
     </div>
@@ -7,7 +7,7 @@
     </div>
 </div>
 
-<div class="mySlide">
+<div class="mySlide frame">
     <div class="priceImage">
         <img src="#" style="width:100%">
     </div>
@@ -16,7 +16,7 @@
     </div>
 </div>
 
-<div class="mySlide">
+<div class="mySlide frame">
     <div class="priceImage">
         <img src="#" style="width:100%">
     </div>
@@ -25,7 +25,7 @@
     </div>
 </div>
 
-<div class="mySlide">
+<div class="mySlide frame">
     <div class="priceImage">
         <img src="#" style="width:100%">
     </div>
@@ -34,7 +34,7 @@
     </div>
 </div>
 
-<div class="mySlide">
+<div class="mySlide frame">
     <div class="priceImage">
         <img src="#" style="width:100%">
     </div>
