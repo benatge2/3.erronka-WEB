2c81127fe0e46a603de39da3988311b6d21bedef
Igor Viyuela <igorviyusan@gmail,com>
2024-03-20 10:55:43 +0100

    Solucionado los problemas del carrusel

--- src/views/supplier/carrusel-Premios.php
+++ src/views/supplier/carrusel-Premios.php
@@ -1,23 +1,19 @@
 <div class="slide-prices">
   <div class="imgContainer">
-    <img src="../../../public/ordenadorFroga.jpg">
-  </div>
-  <div class="slideText">
-    <span>Lorem ipsum dolor, sit amet consectetur adipisicing elite.</span>
-  </div>
-
-  <div class="imgContainer">
-    <img src="../../../public/honor.jpg">
-  </div>
-  <div class="slideText">
-    <span>Lorem ipsum dolor, sit amet consectetur adipisicing elite.</span>
+    <div class="slideText">
+      <span>Lorem ipsum dolor, sit amet consectetur adipisicing elite.</span>
+    </div>
   </div>
 
   <div class="imgContainer">
-    <img src="../../../public/movil3.png">
+    <div class="slideText">
+      <span>Lorem ipsum dolor, sit amet consectetur adipisicing elite.</span>
+    </div>
   </div>
-  <div class="slideText">
-    <span>Lorem ipsum dolor, sit amet consectetur adipisicing elite.</span>
+
+  <div class="imgContainer">
+    <div class="slideText">
+      <span>Lorem ipsum dolor, sit amet consectetur adipisicing elite.</span>
+    </div>
   </div>
-  
 </div>
